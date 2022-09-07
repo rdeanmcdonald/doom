e0487cc4f4f2b018e5b6f04fffe072a119eb85de
Richard McDonald <rmcdonald@wisesystems.com>
2022-09-07 09:13:04 -0400

    bored of font

--- config.el
+++ config.el
@@ -3,8 +3,9 @@
       user-mail-address "richard_mcdonald@alumni.brown.edu"
       ;; doom-font (font-spec :family "FiraCode Nerd Font Mono" :size 22 :weight 'regular)
       ;; doom-variable-pitch-font (font-spec :family "FiraCode Nerd Font" :size 22 :weight 'regular)
-      ;; doom-font (font-spec :family "Iosevka Nerd Font Mono" :size 20 :weight 'regular)
-      doom-font (font-spec :family "FantasqueSansMono Nerd Font" :size 20 :weight 'regular)
+      doom-font (font-spec :family "Iosevka Nerd Font Mono" :size 20 :weight 'regular)
+      ;; doom-font (font-spec :family "FantasqueSansMono Nerd Font" :size 20 :weight 'regular)
+      ;; doom-font (font-spec :family "scientifica" :size 22 :weight 'regular)
       ;; doom-font (font-spec :family "Comic Mono" :size 20 :weight 'regular)
       doom-theme 'doom-dracula
       org-directory "~/org/"
@@ -32,7 +33,7 @@
       lsp-signature-render-documentation t
       lsp-completion-show-detail t
       lsp-completion-show-kind t
-      projectile-project-search-path '("~/random/" "~/wise/" "~/.config/"))
+      projectile-project-search-path '("~/random/" "~/wise/" "~/.config/" "~/chat/"))
 
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
@@ -75,69 +76,14 @@
 ;;         (lambda (frame)
 ;;                 (doom/set-frame-opacity 95)))
 
+;; Don't format on save for markdown
+(setq +format-on-save-enabled-modes
+      '(not gfm-mode))
+
+
 
 ;; Keybindings
 (map! :g "S-<down>" #'+workspace/switch-to)
 ;; Org mode overrides global key bindings
 (with-eval-after-load "org"
   (define-key org-mode-map (kbd "S-<down>") #'+workspace/switch-to))
-;; (map! :leader
-;;       (:prefix ("r" . "Replace")
-;;        :desc "Replace all"
-;;        "r" #'org-journal-search))
-
-;; (use-package! lsp-mode
-;;   :hook  (scala-mode . lsp)
-;;          (lsp-mode . lsp-lens-mode))
-
-;; (use-package! lsp-mode
-;;   ;; Optional - enable lsp-mode automatically in scala files
-;;   :hook  (scala-mode . lsp)
-;;          (lsp-mode . lsp-lens-mode)
-;;   :custom
-;;   ;; what to use when checking on-save. "check" is default, I prefer clippy
-;;   (lsp-rust-analyzer-cargo-watch-command "clippy")
-;;   (lsp-eldoc-render-all t)
-;;   (lsp-idle-delay 0.6)
-;;   ;; This controls the overlays that display type and other hints inline. Enable
-;;   ;; / disable as you prefer. Well require a `lsp-workspace-restart' to have an
-;;   ;; effect on open projects.
-;;   (lsp-rust-analyzer-server-display-inlay-hints t)
-;;   (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
-;;   (lsp-rust-analyzer-display-chaining-hints t)
-;;   (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
-;;   (lsp-rust-analyzer-display-closure-return-type-hints t)
-;;   (lsp-rust-analyzer-display-parameter-hints nil)
-;;   (lsp-rust-analyzer-display-reborrow-hints nil)
-;;   :config
-;;   ;; Uncomment following section if you would like to tune lsp-mode performance according to
-;;   ;; https://emacs-lsp.github.io/lsp-mode/page/performance/
-;;   ;;       (setq gc-cons-threshold 100000000) ;; 100mb
-;;   ;;       (setq read-process-output-max (* 1024 1024)) ;; 1mb
-;;   ;;       (setq lsp-idle-delay 0.500)
-;;   ;;       (setq lsp-log-io nil)
-;;   ;;       (setq lsp-completion-provider :capf)
-;;   (setq lsp-prefer-flymake nil)
-;;   (add-hook 'lsp-mode-hook 'lsp-ui-mode))
-
-;; (use-package! lsp-ui
-;;   :commands lsp-ui-mode
-;;   :custom
-;;   (lsp-ui-peek-always-show t)
-;;   (lsp-ui-sideline-show-hover t)
-;;   (lsp-ui-doc-enable nil))
-
-
-
-
-
-
-
-
-;; (use-package! lsp-metals)
-
-;; (use-package! rustic
-;;   ;; :bind (:map rustic-mode-map
-;;   ;;             ("C-c C-c h" . lsp-describe-thing-at-point))
-;;   :config
-;;   (setq rustic-format-on-save t))
