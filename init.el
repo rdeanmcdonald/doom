6be78ab32fa008f34a224a7a0430e2ea5777f2a6
Richard McDonald <rmcdonald@wisesystems.com>
2022-12-24 09:54:27 -0500

    fsharp

--- init.el
+++ init.el
@@ -132,7 +132,7 @@
        ;;factor
        ;;faust             ; dsp, but you get to keep your soul
        ;;fortran           ; in FORTRAN, GOD is REAL (unless declared INTEGER)
-       ;;fsharp            ; ML stands for Microsoft's Language
+       (fsharp +lsp)            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
